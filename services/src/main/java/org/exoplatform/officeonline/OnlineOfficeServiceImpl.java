fd971eba9dfa547fcf2e9b94cad5b33f83e88acf
VladMartyshevskyi <vladmartishevskii@gmail.com>
2019-06-30 16:41:08 +0300

    Work on WOPI Discovery

--- services/src/main/java/org/exoplatform/officeonline/OnlineOfficeServiceImpl.java
+++ services/src/main/java/org/exoplatform/officeonline/OnlineOfficeServiceImpl.java
@@ -1,28 +1,159 @@
 package org.exoplatform.officeonline;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.PublicKey;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.picocontainer.Startable;
 
 import org.exoplatform.container.xml.InitParams;
+import org.exoplatform.officeonline.WOPIDiscovery.NetZone;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
 
+/**
+ * The Class OnlineOfficeServiceImpl.
+ */
 public class OnlineOfficeServiceImpl implements OnlineOfficeService, Startable {
-  
-  private final String discoveryUrl;
-  
-  public OnlineOfficeServiceImpl(InitParams params) {
-    discoveryUrl = params.getValueParam("discovery-url").getValue();
-  }
-  
 
+  /** The Constant PLACEHOLDER_IS_LICENSED_USER. */
+  public static final String                 PLACEHOLDER_IS_LICENSED_USER       = "IsLicensedUser";
+
+  /** The Constant PLACEHOLDER_IS_LICENSED_USER_VALUE. */
+  public static final String                 PLACEHOLDER_IS_LICENSED_USER_VALUE = "1";
+
+  /** The Constant DISCOVERY_URL_PROPERTY. */
+  private static final String                DISCOVERY_URL_PROPERTY             = "discovery-url";
+
+  /** The Constant LOG. */
+  protected static final Log                 LOG                                =
+                                                 ExoLogger.getLogger(OfficeOnlineEditorServiceImpl.class);
+
+  /** The discovery URL. */
+  private final String                       discoveryURL;
+
+  /** The supported app names. */
+  protected final List<String>               supportedAppNames                  = Arrays.asList("Word", "Excel", "PowerPoint");
+
+  /** The extension action UR ls. */
+  // extension => wopi action => wopi action url
+  protected Map<String, Map<String, String>> extensionActionURLs                = new HashMap<>();
+
+  /** The proof key. */
+  protected PublicKey                        proofKey;
+
+  /** The old proof key. */
+  protected PublicKey                        oldProofKey;
+
+  /**
+   * Instantiates a new online office service impl.
+   *
+   * @param params the params
+   */
+  public OnlineOfficeServiceImpl(InitParams params) {
+    discoveryURL = params.getValueParam(DISCOVERY_URL_PROPERTY).getValue();
+  }
+
+  /**
+   * Start.
+   */
   @Override
   public void start() {
-   
-    
+    try {
+      loadDiscovery();
+    } catch (OfficeOnlineEditorException e) {
+      LOG.error("Error while loading WOPI discovery {}", e.getMessage());
+    }
   }
 
+  /**
+   * Stop.
+   */
   @Override
   public void stop() {
     // TODO Auto-generated method stub
-    
+
+  }
+
+  /**
+   * Load discovery.
+   * @throws OfficeOnlineEditorException 
+   */
+  protected void loadDiscovery() throws OfficeOnlineEditorException {
+    byte[] discoveryBytes = fetchDiscovery();
+    WOPIDiscovery discovery;
+    try {
+      discovery = WOPIDiscovery.read(discoveryBytes);
+    } catch (IOException e) {
+      LOG.error("Error while reading WOPI discovery {}", e.getMessage());
+      return;
+    }
+
+    NetZone netZone = discovery.getNetZone();
+    if (netZone == null) {
+      LOG.error("Invalid WOPI discovery, no net-zone element");
+      return;
+    }
+
+    netZone.getApps().stream().filter(app -> supportedAppNames.contains(app.getName())).forEach(this::registerApp);
+    LOG.debug("Successfully loaded WOPI discovery: WOPI enabled");
+
+    WOPIDiscovery.ProofKey pk = discovery.getProofKey();
+    proofKey = ProofKeyHelper.getPublicKey(pk.getModulus(), pk.getExponent());
+    oldProofKey = ProofKeyHelper.getPublicKey(pk.getOldModulus(), pk.getOldExponent());
+    LOG.debug("Registered proof key: {}", proofKey);
+    LOG.debug("Registered old proof key: {}", oldProofKey);
+  }
+
+  /**
+   * Fetch discovery.
+   *
+   * @return the byte[]
+   */
+  protected byte[] fetchDiscovery() {
+    if (discoveryURL == null) {
+      LOG.warn("No WOPI discovery URL configured, cannot fetch discovery. Please configure the '{}' property.",
+               DISCOVERY_URL_PROPERTY);
+      return ArrayUtils.EMPTY_BYTE_ARRAY;
+    }
+
+    LOG.debug("Fetching WOPI dicovery from discovery URL {}", discoveryURL);
+    HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
+    HttpGet request = new HttpGet(discoveryURL);
+    try (CloseableHttpClient httpClient = httpClientBuilder.build();
+        CloseableHttpResponse response = httpClient.execute(request);
+        InputStream is = response.getEntity().getContent()) {
+      return IOUtils.toByteArray(is);
+    } catch (IOException e) {
+      LOG.error("Error while fetching WOPI discovery: {}", e.getMessage());
+      return ArrayUtils.EMPTY_BYTE_ARRAY;
+    }
+  }
+
+  /**
+   * Register app.
+   *
+   * @param app the app
+   */
+  protected void registerApp(WOPIDiscovery.App app) {
+    app.getActions().forEach(action -> {
+      extensionActionURLs.computeIfAbsent(action.getExt(), k -> new HashMap<>())
+                         .put(action.getName(),
+                              String.format("%s%s=%s&",
+                                            action.getUrl().replaceFirst("<.*$", ""),
+                                            PLACEHOLDER_IS_LICENSED_USER,
+                                            PLACEHOLDER_IS_LICENSED_USER_VALUE));
+    });
   }
 
 }
---
+++ services/src/main/java/org/exoplatform/officeonline/ProofKeyHelper.java
@@ -0,0 +1,81 @@
+package org.exoplatform.officeonline;
+
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
+import java.security.KeyFactory;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.spec.KeySpec;
+import java.security.spec.RSAPublicKeySpec;
+import java.time.Duration;
+import java.time.Instant;
+
+import javax.xml.bind.DatatypeConverter;
+
+
+public class ProofKeyHelper {
+
+  public static final String KEY_FACTORY_ALGORITHM = "RSA";
+
+  public static final String SIGNATURE_ALGORITHM = "SHA256withRSA";
+
+  public static final long EPOCH_IN_TICKS = 621355968000000000L; // January 1, 1970 (start of Unix epoch) in "ticks"
+
+  private ProofKeyHelper() {
+      // helper class
+  }
+
+  public static PublicKey getPublicKey(String modulus, String exponent) throws OfficeOnlineEditorException {
+      BigInteger mod = new BigInteger(1, DatatypeConverter.parseBase64Binary(modulus));
+      BigInteger exp = new BigInteger(1, DatatypeConverter.parseBase64Binary(exponent));
+      KeyFactory factory;
+      try {
+          factory = KeyFactory.getInstance(KEY_FACTORY_ALGORITHM);
+          KeySpec ks = new RSAPublicKeySpec(mod, exp);
+          return factory.generatePublic(ks);
+      } catch (GeneralSecurityException e) {
+          throw new OfficeOnlineEditorException(e);
+      }
+  }
+
+  public static byte[] getExpectedProofBytes(String url, String accessToken, long timestamp) {
+      byte[] accessTokenBytes = accessToken.getBytes(StandardCharsets.UTF_8);
+      byte[] hostUrlBytes = url.toUpperCase().getBytes(StandardCharsets.UTF_8);
+      ByteBuffer byteBuffer = ByteBuffer.allocate(4 + accessTokenBytes.length + 4 + hostUrlBytes.length + 4 + 8);
+      byteBuffer.putInt(accessTokenBytes.length);
+      byteBuffer.put(accessTokenBytes);
+      byteBuffer.putInt(hostUrlBytes.length);
+      byteBuffer.put(hostUrlBytes);
+      byteBuffer.putInt(8);
+      byteBuffer.putLong(timestamp);
+      return byteBuffer.array();
+  }
+
+  public static boolean verifyProofKey(PublicKey key, String proofKeyHeader, byte[] expectedProofBytes) {
+      try {
+          Signature verifier = Signature.getInstance(SIGNATURE_ALGORITHM);
+          verifier.initVerify(key);
+          verifier.update(expectedProofBytes);
+          byte[] signedProof = DatatypeConverter.parseBase64Binary(proofKeyHeader);
+          return verifier.verify(signedProof);
+      } catch (GeneralSecurityException e) {
+          return false;
+      }
+  }
+
+  /**
+   * Checks that the given {@code timestamp} is no more than 20 minutes old.
+   *
+   * @throws NuxeoException if the timestamp is older than 20 minutes
+   */
+  public static boolean verifyTimestamp(long timestamp) {
+      long ticks = timestamp - EPOCH_IN_TICKS; // ticks
+      long ms = ticks / 10_000; // milliseconds
+      Instant instant = Instant.ofEpochMilli(ms);
+      Duration duration = Duration.between(instant, Instant.now());
+      return duration.compareTo(Duration.ofMinutes(20)) <= 0;
+  }
+
+}
