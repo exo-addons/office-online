b9bf9d1f62036583470b2496c2c036c1fecb84b7
VladMartyshevskyi <vladmartishevskii@gmail.com>
2020-01-16 15:35:14 +0200

    Updated accordingly to ECMS

--- services/src/main/java/org/exoplatform/officeonline/documents/OfficeOnlineNewDocumentEditorPlugin.java
+++ services/src/main/java/org/exoplatform/officeonline/documents/OfficeOnlineNewDocumentEditorPlugin.java
@@ -3,8 +3,6 @@ package org.exoplatform.officeonline.documents;
 import javax.jcr.Node;
 
 import org.exoplatform.container.component.BaseComponentPlugin;
-import org.exoplatform.container.xml.InitParams;
-import org.exoplatform.container.xml.ValueParam;
 import org.exoplatform.officeonline.WOPIService;
 import org.exoplatform.services.cms.documents.DocumentTemplate;
 import org.exoplatform.services.cms.documents.NewDocumentEditorPlugin;
@@ -18,14 +16,11 @@ import org.exoplatform.webui.application.WebuiRequestContext;
  */
 public class OfficeOnlineNewDocumentEditorPlugin extends BaseComponentPlugin implements NewDocumentEditorPlugin {
 
-  /** The Constant PROVIDER_PARAM. */
-  protected static final String PROVIDER_PARAM = "provider";
+  /** The Constant PROVIDER_NAME. */
+  protected static final String PROVIDER_NAME = "OfficeOnline";
 
   /** The Constant LOG. */
   protected static final Log    LOG            = ExoLogger.getLogger(OfficeOnlineNewDocumentEditorPlugin.class);
-
-  /** The provider. */
-  protected String              provider;
 
   /** The wopi service. */
   protected final WOPIService   wopiService;
@@ -34,24 +29,9 @@ public class OfficeOnlineNewDocumentEditorPlugin extends BaseComponentPlugin imp
    * Instantiates a new office online new document editor plugin.
    *
    * @param wopiService the wopi service
-   * @param initParams the init params
    */
-  public OfficeOnlineNewDocumentEditorPlugin(WOPIService wopiService, InitParams initParams) {
-    ValueParam providerParam = initParams.getValueParam(PROVIDER_PARAM);
-    if (providerParam != null) {
-      this.provider = providerParam.getValue();
-    }
+  public OfficeOnlineNewDocumentEditorPlugin(WOPIService wopiService) {
     this.wopiService = wopiService;
-  }
-
-  /**
-   * Gets the provider.
-   *
-   * @return the provider
-   */
-  @Override
-  public String getProvider() {
-    return provider;
   }
 
   /**
@@ -87,6 +67,11 @@ public class OfficeOnlineNewDocumentEditorPlugin extends BaseComponentPlugin imp
     WebuiRequestContext requestContext = WebuiRequestContext.getCurrentInstance();
     JavascriptManager js = requestContext.getJavascriptManager();
     js.require("SHARED/officeonline", "officeonline").addScripts("officeonline.initNewDocument();");
+  }
+
+  @Override
+  public String getProviderName() {
+    return PROVIDER_NAME;
   }
 
 }
--- services/src/main/java/org/exoplatform/officeonline/documents/OfficeOnlineNewDocumentTemplatePlugin.java
+++ services/src/main/java/org/exoplatform/officeonline/documents/OfficeOnlineNewDocumentTemplatePlugin.java
@@ -10,6 +10,7 @@ import org.exoplatform.container.xml.InitParams;
 import org.exoplatform.container.xml.ObjectParameter;
 import org.exoplatform.services.cms.documents.DocumentService;
 import org.exoplatform.services.cms.documents.DocumentTemplate;
+import org.exoplatform.services.cms.documents.NewDocumentEditorPlugin;
 import org.exoplatform.services.cms.documents.NewDocumentTemplatePlugin;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
@@ -29,9 +30,6 @@ public class OfficeOnlineNewDocumentTemplatePlugin extends BaseComponentPlugin i
   /** The document types. */
   protected List<DocumentTemplate> templates                        = Collections.emptyList();
 
-  /** The provider. */
-  protected String                 provider;
-
   /** The document service. */
   protected final DocumentService  documentService;
 
@@ -47,22 +45,11 @@ public class OfficeOnlineNewDocumentTemplatePlugin extends BaseComponentPlugin i
       if (obj != null && DocumentService.DocumentTemplatesConfig.class.isAssignableFrom(obj.getClass())) {
         DocumentService.DocumentTemplatesConfig config = DocumentService.DocumentTemplatesConfig.class.cast(obj);
         this.templates = config.getTemplates();
-        this.provider = config.getProvider();
       } else {
         LOG.error("The document templates are not set");
       }
     }
     this.documentService = documentService;
-  }
-
-  /**
-   * Gets the provider.
-   *
-   * @return the provider
-   */
-  @Override
-  public String getProvider() {
-    return provider;
   }
 
   /**
@@ -88,6 +75,20 @@ public class OfficeOnlineNewDocumentTemplatePlugin extends BaseComponentPlugin i
   public Node createDocument(Node parent, String title, DocumentTemplate template) throws Exception {
     LOG.debug("Creating new document {} from template {}", title, template);
     return documentService.createDocumentFromTemplate(parent, title, template);
+  }
+
+  @Override
+  public DocumentTemplate getTemplate(String name) {
+    return templates.stream().filter(t -> t.getName().equals(name)).findFirst().get();
+  }
+
+  @Override
+  public NewDocumentEditorPlugin getEditor() {
+    return documentService.getRegisteredEditorPlugins()
+                   .stream()
+                   .filter(plugin -> plugin.getProviderName().equals(OfficeOnlineNewDocumentEditorPlugin.PROVIDER_NAME))
+                   .findAny()
+                   .orElse(null);
   }
 
 }
